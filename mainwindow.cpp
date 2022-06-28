7d4148e2fc127ec0b51447ab848422e2a386276b
David Lamhauge <davidlamhauge@gmail.com>
2022-06-28 10:45:53 +0200

    Basic UI design

---
+++ mainwindow.cpp
@@ -0,0 +1,17 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent)
+    , ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+
+    connect(ui->btnExit, &QPushButton::clicked, this, &MainWindow::close);
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
