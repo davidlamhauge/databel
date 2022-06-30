30c82de34802f8c2bb5c148030a924e4c6ed812b
David Lamhauge <davidlamhauge@gmail.com>
2022-06-30 23:47:18 +0200

    English translation. Small fixes.

--- lang/daspelling_en.ts
+++ lang/daspelling_en.ts
@@ -5,104 +5,109 @@
     <name>DiviOpgaver</name>
     <message>
         <location filename="../diviopgaver.ui" line="14"/>
-        <source>Division</source>
-        <translation type="unfinished"></translation>
+        <source>Divisions-opgaver</source>
+        <translation>Division tasks</translation>
     </message>
     <message>
-        <location filename="../diviopgaver.ui" line="27"/>
+        <location filename="../diviopgaver.ui" line="21"/>
+        <source>Afslut med ENTER</source>
+        <translation>Answer with ENTER</translation>
+    </message>
+    <message>
+        <location filename="../diviopgaver.ui" line="34"/>
         <source>Antal Opgaver:</source>
-        <translation type="unfinished"></translation>
+        <translation>Number of tasks:</translation>
     </message>
     <message>
-        <location filename="../diviopgaver.ui" line="51"/>
+        <location filename="../diviopgaver.ui" line="58"/>
         <source> af </source>
-        <translation type="unfinished"></translation>
+        <translation> of </translation>
     </message>
     <message>
-        <location filename="../diviopgaver.ui" line="92"/>
+        <location filename="../diviopgaver.ui" line="99"/>
         <source>Antal Rigtige:</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <location filename="../diviopgaver.ui" line="245"/>
-        <source>En gang til</source>
-        <translation type="unfinished"></translation>
+        <translation>Correct answers:</translation>
     </message>
     <message>
         <location filename="../diviopgaver.ui" line="252"/>
+        <source>En gang til</source>
+        <translation>Once more</translation>
+    </message>
+    <message>
+        <location filename="../diviopgaver.ui" line="259"/>
         <source>Afslut</source>
-        <translation type="unfinished"></translation>
+        <translation>Exit</translation>
     </message>
 </context>
 <context>
     <name>GangeOpgaver</name>
     <message>
         <location filename="../gangeopgaver.ui" line="14"/>
-        <source>Form</source>
-        <translation type="unfinished"></translation>
+        <source>Gange-opgaver</source>
+        <translation>Multiplication tasks</translation>
     </message>
     <message>
-        <location filename="../gangeopgaver.ui" line="27"/>
+        <location filename="../gangeopgaver.ui" line="21"/>
+        <source>Afslut med ENTER</source>
+        <translation>Answer with ENTER</translation>
+    </message>
+    <message>
+        <location filename="../gangeopgaver.ui" line="34"/>
         <source>Antal opgaver:</source>
-        <translation type="unfinished"></translation>
+        <translation>Number of tasks:</translation>
     </message>
     <message>
-        <location filename="../gangeopgaver.ui" line="51"/>
+        <location filename="../gangeopgaver.ui" line="58"/>
         <source> af </source>
-        <translation type="unfinished"></translation>
+        <translation> of </translation>
     </message>
     <message>
-        <location filename="../gangeopgaver.ui" line="92"/>
+        <location filename="../gangeopgaver.ui" line="99"/>
         <source>Antal Rigtige:</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <location filename="../gangeopgaver.ui" line="240"/>
-        <source>En gang til</source>
-        <translation type="unfinished"></translation>
+        <translation>Correct answers:</translation>
     </message>
     <message>
         <location filename="../gangeopgaver.ui" line="247"/>
+        <source>En gang til</source>
+        <translation>Once more</translation>
+    </message>
+    <message>
+        <location filename="../gangeopgaver.ui" line="254"/>
         <source>Afslut</source>
-        <translation type="unfinished"></translation>
+        <translation>Exit</translation>
     </message>
 </context>
 <context>
     <name>MainWindow</name>
     <message>
-        <location filename="../mainwindow.ui" line="14"/>
-        <source>MainWindow</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <location filename="../mainwindow.ui" line="67"/>
+        <location filename="../mainwindow.ui" line="71"/>
         <source>Plus</source>
-        <translation type="unfinished"></translation>
+        <translation>Addition</translation>
     </message>
     <message>
-        <location filename="../mainwindow.ui" line="77"/>
+        <location filename="../mainwindow.ui" line="81"/>
         <source>Minus</source>
-        <translation type="unfinished"></translation>
+        <translation>Subtraction</translation>
     </message>
     <message>
-        <location filename="../mainwindow.ui" line="84"/>
+        <location filename="../mainwindow.ui" line="88"/>
         <source>Gange</source>
-        <translation type="unfinished"></translation>
+        <translation>Multiplication</translation>
     </message>
     <message>
-        <location filename="../mainwindow.ui" line="91"/>
+        <location filename="../mainwindow.ui" line="95"/>
         <source>Division</source>
-        <translation type="unfinished"></translation>
+        <translation>Division</translation>
     </message>
     <message>
-        <location filename="../mainwindow.ui" line="100"/>
+        <location filename="../mainwindow.ui" line="104"/>
         <source>Valgmuligheder</source>
-        <translation type="unfinished"></translation>
+        <translation>Options</translation>
     </message>
     <message>
-        <location filename="../mainwindow.ui" line="124"/>
+        <location filename="../mainwindow.ui" line="128"/>
         <source>Afslut</source>
-        <translation type="unfinished"></translation>
+        <translation>Exit</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="65"/>
@@ -110,7 +115,7 @@
         <location filename="../mainwindow.cpp" line="163"/>
         <location filename="../mainwindow.cpp" line="206"/>
         <source>Antal opgaver:</source>
-        <translation type="unfinished"></translation>
+        <translation>Number of tasks:</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="71"/>
@@ -118,68 +123,68 @@
         <location filename="../mainwindow.cpp" line="169"/>
         <location filename="../mainwindow.cpp" line="212"/>
         <source>Interval: 10-100</source>
-        <translation type="unfinished"></translation>
+        <translation>Interval: 10-100</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="75"/>
         <source>Maksimum sum:</source>
-        <translation type="unfinished"></translation>
+        <translation>Maximum sum:</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="81"/>
         <location filename="../mainwindow.cpp" line="222"/>
         <source>Interval: 20-1000</source>
-        <translation type="unfinished"></translation>
+        <translation>Interval: 20-1000</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="85"/>
         <source>Gå til Plus-opgaver</source>
-        <translation type="unfinished"></translation>
+        <translation>Go to Plus tasks</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="118"/>
         <source>Ikke tal større end:</source>
-        <translation type="unfinished"></translation>
+        <translation>No number greater than:</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="124"/>
         <source>Interval: 20-500</source>
-        <translation type="unfinished"></translation>
+        <translation>Interval: 20-500</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="128"/>
         <source>Tillad negativ differens</source>
-        <translation type="unfinished"></translation>
+        <translation>Allow negative difference</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="131"/>
         <source>Gå til Minus-opgaver</source>
-        <translation type="unfinished"></translation>
+        <translation>Go to Minus tasks</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="173"/>
         <source>Ikke faktorer større end:</source>
-        <translation type="unfinished"></translation>
+        <translation>No factors greater than:</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="179"/>
         <source>Interval: 5-20</source>
-        <translation type="unfinished"></translation>
+        <translation>Interval: 5-20</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="183"/>
         <source>Gå til Gange-opgaver</source>
-        <translation type="unfinished"></translation>
+        <translation>bo to Multiplication tasks</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="216"/>
         <source>Ikke dividend større end:</source>
-        <translation type="unfinished"></translation>
+        <translation>No dividend greater than:</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="226"/>
         <source>Gå til Divisions-opgaver</source>
-        <translation type="unfinished"></translation>
+        <translation>Go to division tasks</translation>
     </message>
 </context>
 <context>
@@ -187,76 +192,81 @@
     <message>
         <location filename="../minusopgaver.ui" line="14"/>
         <source>Minus-opgaver</source>
-        <translation type="unfinished"></translation>
+        <translation>Minus tasks</translation>
     </message>
     <message>
-        <location filename="../minusopgaver.ui" line="27"/>
+        <location filename="../minusopgaver.ui" line="21"/>
+        <source>Afslut med ENTER</source>
+        <translation>Answer with ENTER</translation>
+    </message>
+    <message>
+        <location filename="../minusopgaver.ui" line="34"/>
         <source>Antal opgaver:</source>
-        <translation type="unfinished"></translation>
+        <translation>Number of tasks:</translation>
     </message>
     <message>
-        <location filename="../minusopgaver.ui" line="51"/>
-        <location filename="../minusopgaver.ui" line="116"/>
+        <location filename="../minusopgaver.ui" line="58"/>
+        <location filename="../minusopgaver.ui" line="123"/>
         <source>af</source>
-        <translation type="unfinished"></translation>
+        <translation>of</translation>
     </message>
     <message>
-        <location filename="../minusopgaver.ui" line="92"/>
+        <location filename="../minusopgaver.ui" line="99"/>
         <source>Antal rigtige:</source>
-        <translation type="unfinished"></translation>
+        <translation>Correct answers:</translation>
     </message>
     <message>
-        <location filename="../minusopgaver.ui" line="218"/>
+        <location filename="../minusopgaver.ui" line="225"/>
         <source>0</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <location filename="../minusopgaver.ui" line="238"/>
-        <source>En gang til</source>
-        <translation type="unfinished"></translation>
+        <translation>0</translation>
     </message>
     <message>
         <location filename="../minusopgaver.ui" line="245"/>
+        <source>En gang til</source>
+        <translation>Once more</translation>
+    </message>
+    <message>
+        <location filename="../minusopgaver.ui" line="252"/>
         <source>Afslut</source>
-        <translation type="unfinished"></translation>
+        <translation>Exit</translation>
     </message>
 </context>
 <context>
     <name>PlusOpgaver</name>
     <message>
         <location filename="../plusopgaver.ui" line="14"/>
-        <source>Plus</source>
-        <translation type="unfinished"></translation>
+        <source>Plus-opgaver</source>
+        <translation>Plus tasks</translation>
     </message>
     <message>
-        <location filename="../plusopgaver.ui" line="27"/>
+        <location filename="../plusopgaver.ui" line="31"/>
         <source>Antal opgaver:</source>
-        <translation type="unfinished"></translation>
+        <translation>Number of tasks:</translation>
     </message>
     <message>
-        <location filename="../plusopgaver.ui" line="51"/>
+        <location filename="../plusopgaver.ui" line="55"/>
         <source> af </source>
-        <translation type="unfinished"></translation>
+        <translation> of </translation>
     </message>
     <message>
-        <location filename="../plusopgaver.ui" line="92"/>
+        <location filename="../plusopgaver.ui" line="96"/>
         <source>Antal rigtige:</source>
-        <translation type="unfinished"></translation>
+        <translation>Correct answers:</translation>
     </message>
     <message>
-        <location filename="../plusopgaver.ui" line="205"/>
+        <location filename="../plusopgaver.ui" line="209"/>
         <source>Afslut med ENTER</source>
-        <translation type="unfinished"></translation>
+        <translation>Answer with ENTER</translation>
     </message>
     <message>
-        <location filename="../plusopgaver.ui" line="261"/>
+        <location filename="../plusopgaver.ui" line="265"/>
         <source>En gang til</source>
-        <translation type="unfinished"></translation>
+        <translation>Once more</translation>
     </message>
     <message>
-        <location filename="../plusopgaver.ui" line="268"/>
+        <location filename="../plusopgaver.ui" line="272"/>
         <source>Afslut</source>
-        <translation type="unfinished"></translation>
+        <translation>Exit</translation>
     </message>
 </context>
 </TS>
